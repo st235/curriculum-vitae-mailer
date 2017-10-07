2969548180134a8068fd9e3e18cf534fbbf65a9c
Alexander Dadukin <st235@yandex.ru>
2017-10-07 16:46:54 +0300

    Update App.kt

--- src/main/kotlin/com/github/sasd97/mailer/App.kt
+++ src/main/kotlin/com/github/sasd97/mailer/App.kt
@@ -2,7 +2,10 @@ package com.github.sasd97.mailer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.SpringApplication
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 open class App {
@@ -11,6 +14,15 @@ open class App {
         @JvmStatic
         fun main(args: Array<String>) {
             SpringApplication.run(App::class.java, *args)
+        }
+    }
+
+    @Bean
+    open fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry?) {
+                registry!!.addMapping("/email/send").allowedOrigins("https://st235.github.io")
+            }
         }
     }
 }
